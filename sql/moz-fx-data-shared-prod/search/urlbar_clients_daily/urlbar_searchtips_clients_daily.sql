ff97df97cf601bb26f0f518db4bc98c1b7605cf7
Teon L Brooks <teon.brooks@gmail.com>
2021-02-24 18:27:29 -0500

    Use coalesce
    Co-authored-by: Ben Wu <benjaminwu124@gmail.com>

--- sql/moz-fx-data-shared-prod/search/urlbar_clients_daily/urlbar_searchtips_clients_daily.sql
+++ sql/moz-fx-data-shared-prod/search/urlbar_clients_daily/urlbar_searchtips_clients_daily.sql
@@ -31,7 +31,7 @@ WITH
     client_id,
     STRUCT(ARRAY_AGG(tip) AS key,
       ARRAY_AGG(n_engagements) AS value) AS searchtip_engagement_count_by_type,
-    SUM(n_engagements) AS searchtip_engagement_count
+    COALESCE(SUM(n_engagements), 0) AS searchtip_engagement_count
   FROM
     searchtips_clients_daily_temp
   WHERE
