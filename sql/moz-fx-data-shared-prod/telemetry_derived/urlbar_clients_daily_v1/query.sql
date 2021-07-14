da974430fbcf2c34d9be5b09370b155707f6e410
Teon L Brooks <teon.brooks@gmail.com>
2021-07-14 16:49:14 -0400

    Update query.sql

--- sql/moz-fx-data-shared-prod/telemetry_derived/urlbar_clients_daily_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/urlbar_clients_daily_v1/query.sql
@@ -47,7 +47,7 @@ with temp_picked as (
     client_id,
     mozfun.map.sum(
       ARRAY_AGG(
-        STRUCT(probe_name AS key, probe_searchmode.value AS value)
+        STRUCT(searchmode_type AS key, probe_searchmode.value AS value)
       )
     ) AS searchmode_by_entry,
     mozfun.map.sum(
@@ -59,19 +59,19 @@ with temp_picked as (
     mozdata.telemetry.clients_daily
   CROSS JOIN
     UNNEST([
-      STRUCT("bookmarkmenu" AS probe_name, scalar_parent_urlbar_searchmode_bookmarkmenu_sum AS probe),
-      STRUCT("handoff" AS probe_name, scalar_parent_urlbar_searchmode_handoff_sum AS probe),
-      STRUCT("keywordoffer" AS probe_name, scalar_parent_urlbar_searchmode_keywordoffer_sum AS probe),
-      STRUCT("oneoff" AS probe_name, scalar_parent_urlbar_searchmode_oneoff_sum AS probe),
-      STRUCT("other" AS probe_name, scalar_parent_urlbar_searchmode_other_sum AS probe),
-      STRUCT("shortcut" AS probe_name, scalar_parent_urlbar_searchmode_shortcut_sum AS probe),
-      STRUCT("tabmenu" AS probe_name, scalar_parent_urlbar_searchmode_tabmenu_sum AS probe),
-      STRUCT("tabtosearch" AS probe_name, scalar_parent_urlbar_searchmode_tabtosearch_sum AS probe),
-      STRUCT("tabtosearch_onboard" AS probe_name, scalar_parent_urlbar_searchmode_tabtosearch_onboard_sum AS probe),
-      STRUCT("topsites_newtab" AS probe_name, scalar_parent_urlbar_searchmode_topsites_newtab_sum AS probe),
-      STRUCT("topsites_urlbar" AS probe_name, scalar_parent_urlbar_searchmode_topsites_urlbar_sum AS probe),
-      STRUCT("touchbar" AS probe_name, scalar_parent_urlbar_searchmode_touchbar_sum AS probe),
-      STRUCT("typed" AS probe_name, scalar_parent_urlbar_searchmode_typed_sum AS probe)
+      STRUCT("bookmarkmenu" AS searchmode_type, scalar_parent_urlbar_searchmode_bookmarkmenu_sum AS probe),
+      STRUCT("handoff" AS searchmode_type, scalar_parent_urlbar_searchmode_handoff_sum AS probe),
+      STRUCT("keywordoffer" AS searchmode_type, scalar_parent_urlbar_searchmode_keywordoffer_sum AS probe),
+      STRUCT("oneoff" AS searchmode_type, scalar_parent_urlbar_searchmode_oneoff_sum AS probe),
+      STRUCT("other" AS searchmode_type, scalar_parent_urlbar_searchmode_other_sum AS probe),
+      STRUCT("shortcut" AS searchmode_type, scalar_parent_urlbar_searchmode_shortcut_sum AS probe),
+      STRUCT("tabmenu" AS searchmode_type, scalar_parent_urlbar_searchmode_tabmenu_sum AS probe),
+      STRUCT("tabtosearch" AS searchmode_type, scalar_parent_urlbar_searchmode_tabtosearch_sum AS probe),
+      STRUCT("tabtosearch_onboard" AS searchmode_type, scalar_parent_urlbar_searchmode_tabtosearch_onboard_sum AS probe),
+      STRUCT("topsites_newtab" AS searchmode_type, scalar_parent_urlbar_searchmode_topsites_newtab_sum AS probe),
+      STRUCT("topsites_urlbar" AS searchmode_type, scalar_parent_urlbar_searchmode_topsites_urlbar_sum AS probe),
+      STRUCT("touchbar" AS searchmode_type, scalar_parent_urlbar_searchmode_touchbar_sum AS probe),
+      STRUCT("typed" AS searchmode_type, scalar_parent_urlbar_searchmode_typed_sum AS probe)
     ])
   CROSS JOIN
     UNNEST(probe) AS probe_searchmode
@@ -81,7 +81,6 @@ with temp_picked as (
     submission_date,
     client_id
 )
-  -- build it all up
 SELECT
   *
 FROM
